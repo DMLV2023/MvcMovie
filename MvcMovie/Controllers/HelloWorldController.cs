8620b997daab3d1f03da493003cf863fa2907daa
DMLV2023 <d.lavalle.sistemas@gmail.com>
2024-05-02 16:01:02 -0300

    Semana 3

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,22 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
+
 namespace MvcMovie.Controllers
 {
-    [Route("HelloWorld")]
     public class HelloWorldController : Controller
     {
-        [HttpGet("Index")]
-        public string Index()
+        public IActionResult Index()
         {
-            return "Acción por defecto...";
+            return View();
         }
 
-        [HttpGet("Welcome/{name}")]
-        public string Welcome(string name, int id = 1)
+
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            var encoder = HtmlEncoder.Default;
-            return encoder.Encode($"Hello {name}, ID: {id}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+
+            return View();
         }
     }
 }
